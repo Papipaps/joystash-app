ae0873d3942b4a88803cee197bb239f94e366b35
Papipaps <jordanpeltro@gmail.com>
2023-04-03 19:23:23 +0200

    fix tech icons in card

--- src/assets/tech/techimages-export.ts
+++ src/assets/tech/techimages-export.ts
@@ -1,14 +1,25 @@
+import elasticsearch from "./elasticsearch.png";
+import docker from "./docker.png";
+import atlassion from "./atlassian.png";
+import java from "./java.png";
+import jenkins from "./jenkins.png";
+import mongodb from "./mongodb.png";
+import neo4j from "./neo4j.png";
+import pgsql from "./pgsql.png";
+import react from "./react.png";
+import spring from "./spring.svg";
+import ts from "./ts.png";
 const TechIcons = {
-    elasticsearch: "src/assets/tech/elasticsearch.png",
-    docker: "src/assets/tech/docker.png",
-    atlassion: "src/assets/tech/atlassian.png",
-    java: "src/assets/tech/java.png",
-    jenkins: "src/assets/tech/jenkins.png",
-    mongodb: "src/assets/tech/mongodb.png",
-    neo4j: "src/assets/tech/neo4j.png",
-    pgsql: "src/assets/tech/pgsql.png",
-    react: "src/assets/tech/react.png",
-    spring: "src/assets/tech/spring.svg",
-    ts: "src/assets/tech/ts.png",
-}
-export default TechIcons
+  elasticsearch: elasticsearch,
+  docker: docker,
+  atlassion: atlassion,
+  java: java,
+  jenkins: jenkins,
+  mongodb: mongodb,
+  neo4j: neo4j,
+  pgsql: pgsql,
+  react: react,
+  spring: spring,
+  ts: ts,
+};
+export default TechIcons;
