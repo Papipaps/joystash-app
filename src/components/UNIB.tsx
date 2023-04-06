cb5cdd5052c51d53a7078e9ab456105d3b5948dd
Papipaps <jordanpeltro@gmail.com>
2023-04-06 12:47:14 +0200

    update fighters images

--- src/components/UNIB.tsx
+++ src/components/UNIB.tsx
@@ -6,26 +6,26 @@ import { SelectionContext } from "./selection-provider";
 function UNIB() {
   const { selected } = useContext(SelectionContext);
   const characters = [
-    { id: 1, name: "Zeh-ra", img: Fighters.fighter1 },
+    { id: 1, name: "Huh-Long", img: Fighters.fighter1 },
     { id: 2, name: "Tara", img: Fighters.fighter2 },
     { id: 3, name: "Charlie", img: Fighters.fighter3 },
-    { id: 4, name: "Steve", img: Fighters.fighter4 },
-    { id: 5, name: "Mr.Bordannov", img: Fighters.fighter5 },
+    { id: 4, name: "Grace", img: Fighters.fighter4 },
+    { id: 5, name: "Kaitlyn", img: Fighters.fighter5 },
     { id: 6, name: "Taliyah", img: Fighters.fighter6 },
     { id: 7, name: "Grace", img: Fighters.fighter7 },
     { id: 8, name: "Hery", img: Fighters.fighter8 },
     { id: 9, name: "Isabelle", img: Fighters.fighter9 },
     { id: 10, name: "Jade Hu", img: Fighters.fighter10 },
-    { id: 11, name: "Kaitlyn Smith", img: Fighters.fighter11 },
-    { id: 12, name: "Aeden Smith", img: Fighters.fighter12 },
+    { id: 11, name: "Bordannov", img: Fighters.fighter11 },
+    { id: 12, name: "Aedenh", img: Fighters.fighter12 },
     { id: 13, name: "Mia", img: Fighters.fighter13 },
-    { id: 14, name: "Nathania", img: Fighters.fighter14 },
-    { id: 15, name: "Olivio", img: Fighters.fighter15 },
+    { id: 14, name: "Nathan", img: Fighters.fighter14 },
+    { id: 15, name: "Oliver", img: Fighters.fighter15 },
     { id: 16, name: "Rivaï", img: Fighters.fighter16 },
-    { id: 17, name: "Patrick", img: Fighters.fighter17 },
-    { id: 18, name: "Quinn", img: Fighters.fighter18 },
-    { id: 19, name: "Riley", img: Fighters.fighter19 },
-    { id: 20, name: "Samuel", img: Fighters.fighter20},
+    { id: 17, name: "Rick", img: Fighters.fighter17 },
+    { id: 18, name: "Neewa", img: Fighters.fighter18 },
+    { id: 19, name: "Ildia", img: Fighters.fighter19 },
+    { id: 20, name: "Sarros", img: Fighters.fighter20},
   ];
   
    const character: Character | undefined = characters.find(
