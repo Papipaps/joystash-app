157b3de452118b0f9d69b1328bc5afd87f91a056
Papipaps <jordanpeltro@gmail.com>
2023-05-23 18:53:21 +0200

    frefezg

--- src/components/organisms/Navigation/Navbar.tsx
+++ src/components/organisms/Navigation/Navbar.tsx
@@ -16,13 +16,28 @@ function Navbar() {
     const location = useLocation();
     return (
       <ul className={styles.tabs}>
-        <li className={`${location.pathname === "/" && styles.active}`}>
+        <li
+          className={[
+            styles.item,
+            location.pathname === "/" && styles.active,
+          ].join(" ")}
+        >
           <Link to={"/"}>Home</Link>
         </li>
-        <li className={`${location.pathname === "/showcase" && styles.active}`}>
+        <li
+          className={[
+            styles.item,
+            location.pathname === "/showcase" && styles.active,
+          ].join(" ")}
+        >
           <Link to={"/showcase"}>Showcase</Link>
         </li>
-        <li className={`${location.pathname === "/contact" && styles.active}`}>
+        <li
+          className={[
+            styles.item,
+            location.pathname === "/contact" && styles.active,
+          ].join(" ")}
+        >
           <Link to={"/contact"}>Contact</Link>
         </li>
       </ul>
